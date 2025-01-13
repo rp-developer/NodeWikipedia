aaf888d16e1d9f5f4ac8f4deb9a110712d96aecb
Ryan Pfeiffer <ryanpfeiffer@Ryans-MacBook-Air.local>
2025-01-12 20:00:30 -0500

    bug and feature improvements

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app = express();
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
-const client = createClient ({
-    url : "rediss://default:AdhiAAIjcDE1OGUzNzEzOTUxYjM0MDRmODkyNWQ0NDczYzI4YzUxNXAxMA@perfect-caribou-55394.upstash.io:6379" || "redis://localhost:6379"
-  });
+const client = createClient( {
+    url: process.env.REDIS_URL || 'redis://localhost:6379',
+});
 client.on('error', (err) => console.log(`Redis Client Error`, err));
 client.connect()
 const port = 3000
